5bd81488f26020eaa07aa00ed6430184faad04e1
Liuhala Esamatti <esamatti.liuhala@veikkaus.fi>
2025-03-27 14:54:54 +0200

    eki: use fixed qr code parameters errorCorrectionLevel M and version 3

--- eki/lib/eki/qrCode.ts
+++ eki/lib/eki/qrCode.ts
@@ -7,7 +7,7 @@ export function createQrCode(ticketId: string) {
   const dateTime = DateTime.now()
   const filename = `${dateTime.toFormat('yyyyMMddHHmmss')}_${ticketId}.png`
   console.log(color.magenta(`Creating QR-code: ${filename}`))
-  toDataURL(ticketId).then(url => {
+  toDataURL(ticketId, { errorCorrectionLevel: 'M', version: 3}).then(url => {
     fs.writeFileSync(`./${filename}`, Buffer.from(url.split(',')[1], 'base64'))
   })
 }  
